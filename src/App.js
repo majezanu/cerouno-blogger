73d8aec58cad73d919da73055b0b202e6922b0a1
Manuel Zavala <majezanu@gmail.com>
2020-02-07 11:01:46 -0600

    feat: create home page

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import React from 'react';
-import NavBar from './components/molecules/NavBar'
+import Home from './components/pages/Home';
 import './App.css';
+
 function App() {
   return (
     <div className="App">
-      <NavBar></NavBar>
+      <Home></Home>
     </div>
   );
 }
---
+++ src/components/pages/Home/Home.js
@@ -0,0 +1,24 @@
+import React, { Component } from 'react';
+import NavBar from '../../molecules/NavBar'
+import BlogCard from '../../molecules/BlogCard'
+class Home extends Component {
+    constructor(props) {
+        super(props);
+        this.links = [
+            {text: 'Servicios', className: 'nav-link text-primary', url: 'http://www.google.com'},
+            {text: 'Acerca de', className: 'nav-link text-danger', url: 'http://www.google.com'},
+            {text: 'Contacto', className: 'nav-link text-success', url: 'http://www.google.com'}
+        ];
+    }
+
+    render() {
+        return (
+            <div>
+                <NavBar links={this.links}></NavBar>
+                <BlogCard></BlogCard>
+            </div>
+        );
+    }
+}
+
+export default Home
