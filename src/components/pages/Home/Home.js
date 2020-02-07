0ac191fb9db8e7d45943313a1eef925c57c870f4
Manuel Zavala <majezanu@gmail.com>
2020-02-07 17:29:17 -0600

    feat: add mock for Home

--- src/components/pages/Home/Home.js
+++ src/components/pages/Home/Home.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
 import NavBar from '../../molecules/NavBar'
 import BlogCard from '../../molecules/BlogCard'
+import BloogFeed from '../../organisms/BlogFeed/BloogFeed';
+import {homeMock} from './Home.mock';
 class Home extends Component {
     constructor(props) {
         super(props);
@@ -9,6 +11,7 @@ class Home extends Component {
             {text: 'Acerca de', className: 'nav-link text-danger', url: 'http://www.google.com'},
             {text: 'Contacto', className: 'nav-link text-success', url: 'http://www.google.com'}
         ];
+        this.data = homeMock;
     }
 
     render() {
@@ -16,6 +19,13 @@ class Home extends Component {
             <div>
                 <NavBar links={this.links}></NavBar>
                 <BlogCard style={`banner`}></BlogCard>
+                <div className='container'>
+                    <div className="row">
+                        {this.data.feeds.map((feed, index) => {
+                            return <BloogFeed key={index} feed={feed} style={`listed`}></BloogFeed>
+                        })}
+                    </div>
+                </div>
             </div>
         );
     }
---
+++ src/components/pages/Home/Home.mock.js
@@ -0,0 +1,109 @@
+const homeMock = {
+    links: [
+        {text: 'Servicios', className: 'nav-link text-primary', url: 'http://www.google.com'},
+        {text: 'Acerca de', className: 'nav-link text-danger', url: 'http://www.google.com'},
+        {text: 'Contacto', className: 'nav-link text-success', url: 'http://www.google.com'}
+    ],
+    feeds: [
+        {
+            blogs: [
+                {
+                    title: '6 video juegos de terror que no dejarás de jugar',
+                    date: new Date(),
+                    image: {
+                        src: 'https://cdn02.nintendo-europe.com/media/images/06_screenshots/games_5/nintendo_switch_download_software_2/nswitchds_outlastbundleofterror/NSwitchDS_OutlastBundleOfTerror_06.jpg',
+                        alt: 'Outlast',
+                    },
+                    category: 'Video juegos'
+                },
+                {
+                    title: 'Bornout 3: El mejor video juego de carreras',
+                    date: new Date(),
+                    image: {
+                        src: 'https://wallpaperstock.net/wallpapers/thumbs1/18474.jpg',
+                        alt: 'burnout3',
+                    },
+                    category: 'Video juegos'
+                },
+                {
+                    title: 'Historia de los video juegos',
+                    date: new Date(),
+                    image: {
+                        src: 'https://www.fib.upc.edu/retro-informatica/historia/videojocs/mainColumnParagraphs/0/image/oxo.jpg',
+                        alt: 'history',
+                    },
+                    category: 'Video juegos'
+                },
+            ],
+            category: 'Video juegos'
+        },
+        {
+            blogs: [
+                {
+                    title: 'El mejor show del medio tiempo',
+                    date: new Date(),
+                    image: {
+                        src: 'https://i.ytimg.com/vi/CHWRqiljo8M/hqdefault.jpg',
+                        alt: 'KatyPerry',
+                    },
+                    category: 'Música'
+                },
+                {
+                    title: 'Michael Jackson: El mejor artista de todos los tiempos',
+                    date: new Date(),
+                    image: {
+                        src: 'https://s1.eestatic.com/2019/06/28/reportajes/Reportajes_409721607_126765188_1024x576.jpg',
+                        alt: 'MichaelJackson',
+                    },
+                    category: 'Música'
+                },
+                {
+                    title: 'The Beatles: Ascenso y Caída',
+                    date: new Date(),
+                    image: {
+                        src: 'https://lh3.googleusercontent.com/proxy/HSJv1oxnj3GXM3btfWGwfqC0SQdI7EARq9deNZWgeFxpSsR2QxtVvIFgdj7CqgF5ctN2RXdhvEljrx_0bz-b-UPtKeR7-qTNUMZ4GZqsF3ecGublwOV8skqfzFIvyytFZGo6G6-I83fmY7_dx0GSZq8LxHgErLRIG7eQeSHpUS4pYZg4ig78C5jUSaONkQY',
+                        alt: 'Beatles',
+                    },
+                    category: 'Música'
+                },
+            ],
+            category: 'Música'
+        },
+        {
+            blogs: [
+                {
+                    title: '6 video juegos de terror que no dejarás de jugar',
+                    date: new Date(),
+                    image: {
+                        src: 'https://cdn02.nintendo-europe.com/media/images/06_screenshots/games_5/nintendo_switch_download_software_2/nswitchds_outlastbundleofterror/NSwitchDS_OutlastBundleOfTerror_06.jpg',
+                        alt: 'Outlast',
+                    },
+                    category: 'Video juegos'
+                },
+                {
+                    title: 'Bornout 3: El mejor video juego de carreras',
+                    date: new Date(),
+                    image: {
+                        src: 'https://wallpaperstock.net/wallpapers/thumbs1/18474.jpg',
+                        alt: 'burnout3',
+                    },
+                    category: 'Video juegos'
+                },
+                {
+                    title: 'Historia de los video juegos',
+                    date: new Date(),
+                    image: {
+                        src: 'https://www.fib.upc.edu/retro-informatica/historia/videojocs/mainColumnParagraphs/0/image/oxo.jpg',
+                        alt: 'history',
+                    },
+                    category: 'Video juegos'
+                },
+            ],
+            category: 'Video juegos'
+        }
+    ]
+}
+
+export {
+    homeMock
+}
