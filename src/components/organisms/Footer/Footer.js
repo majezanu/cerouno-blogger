93efc882f37b1e1a497685763a3f0c3d81550a37
Manuel Zavala <majezanu@gmail.com>
2020-02-07 22:49:37 -0600

    feat: create Footer component and add on Home page

---
+++ src/components/organisms/Footer/Footer.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+import SubscribeInput from '../../molecules/SubscribeInput';
+class Footer extends Component {
+  render() {
+    return (
+        <footer className="page-footer font-small bg-dark pt-4">
+            <SubscribeInput></SubscribeInput>
+            <div className="container-fluid text-center text-md-left text-white">
+                <div className="footer-copyright text-center py-3">
+                    © 2020 Copyright
+                </div>
+            </div>
+        </footer>
+        
+    );
+  }
+}
+
+export default Footer
